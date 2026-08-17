6aacc3b3e5481be444fe026f6408716c0de75416
Marcus Rosales <marosale@Physicss-MacBook-Pro.local>
2026-08-17 13:11:32 -0400

    ABI breaking repo. Needs to be run with two different pythons

---
+++ ABI_breaks/build/CMakeFiles/mymodule.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mymodule.dir/mymodule2.cpp.o"
+  "CMakeFiles/mymodule.dir/mymodule2.cpp.o.d"
+  "abi_result.pdb"
+  "abi_result.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mymodule.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
